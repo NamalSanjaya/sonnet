261ea5aa153ff79e1f627ad4e5ea015068cb4050
NamalSanjaya <68647363+NamalSanjaya@users.noreply.github.com>
2022-05-20 22:05:39 +0530

    Add db client creation

--- common/types/types.go
+++ common/types/types.go
@@ -1,8 +1,13 @@
 package types
 
 type DataUnit struct {
-	Id string
+	Id         string
 	InsertedAt string
-	Data []byte
-	Size int        // size in byte
+	Data       []byte
+	Size       int // size in byte
+}
+
+type DbConfig struct {
+	Schema, Username, Password, Server, Database string
+	Port                                         int
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/NamalSanjayaFernando/sonnet
 
 go 1.17
+
+require github.com/jmoiron/sqlx v1.3.5
